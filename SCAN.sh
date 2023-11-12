631bb63b3ce7839f8c7ceeb252f5035045019ffa
LinuxUser111 <150618164+LinuxUser111@users.noreply.github.com>
2023-11-12 14:01:20 -0500

    Create SCAN.sh

---
+++ SCAN.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+echo "Enter the starting Ip address :"
+read FirstIp
+
+echo "Enter the last Ip address :"
+
+read LastIp
+
+echo "Enter the port number you want to scan for :"
+
+read port
+
+nmap -sT $FirstIp- $LastIp -p $port -oG web
+cat web | grep open >web1
+cat web1 |cut -f2 -d":" | cut -f1 -d"(" > web2
+cat web2
+
+
+
+
+
